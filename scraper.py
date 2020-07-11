f93cd069468e9d5fcb79a7aeac25810635842264
JhaPrashant1108 <prashant.jha0008@gmail.com>
2020-07-11 17:45:39 +0530

    bug fixes

--- scraper.py
+++ scraper.py
@@ -12,22 +12,19 @@ imageUrl = "https://imgflip.com/s/meme/"
 
 for i in range(21):
     getUrl = url+str(i+1)
-    print(getUrl)
     htmlContent = requests.get(getUrl).content
     soup = BeautifulSoup(htmlContent,'html.parser')
     for imgLinks in soup.findAll("a",{"class":"mt-caption l but"}):
         name = imgLinks['title'].replace(" Meme Generator","")
         link = imageUrl+imgLinks['href'].split("/")[2]+".jpg"
-        print(name+":-"+link)
         imgLinkDict.update({name:link})
 
 	
 
 
 	
-
-out_file = open("imgLinks.json", "w") 
+outFile = open("imgLinks.json", "w") 
 	
-json.dump(imgLinkDict, out_file, indent = 4) 
+json.dump(imgLinkDict, outFile, indent = 4) 
 	
-out_file.close() 
+outFile.close() 
